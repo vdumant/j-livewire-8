0f06d538965e442424b30f45b8218564f4a30674
Victor Duman <duman.fabian.g@gmail.com>
2022-01-07 11:09:00 -0500

    primer commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.web')
+
+@section('content')
+    <div class="text-center">
+        <h1 class="text-3xl text-gray-700 mb-2 uppercase">Ultimos cursos</h1>
+        <h2 class="text-xl text-gray-600">Formate online como profesional</h2>
+        <h3 class="text-lg text-gray-600">70% de los graduados duplica sus ingresos</h3>
+    </div>
+
+    <livewire:course-list>
+@endsection
